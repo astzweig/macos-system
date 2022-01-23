a80e1ed88facad04df40649f279f2b96d10ec36a
Rezart Qelibari <entwickler-kontakt@astzweig.de>
2022-01-23 01:13:48 +0100

    Run git quietly

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ function main() {
   local tmddir="`mktemp -d -t 'macos-system'`"
   trap 'rm -fr -- "${tmpdir}"' EXIT
   pushd -q "${tmddir}"
-  git clone --recurse-submodules https://github.com/astzweig/macos-system.git .
+  git clone -q --recurse-submodules https://github.com/astzweig/macos-system.git .
   ./install.sh
   popd -q
 }
