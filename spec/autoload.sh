366047fb2075b023b51b099b627bb6029e370ac4
Rezart Qelibari <entwickler-kontakt@astzweig.de>
2022-02-04 14:10:19 +0100

    Autoload zshlib functions for shellspec

--- spec/autoload.sh
+++ spec/autoload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 FPATH="`pwd`/zshlib:${FPATH}"
-local funcNames=("${(@f)$(find . -type f -perm +u=x -maxdepth 1 | awk -F/ '{ print $NF }')}")
+local funcNames=("${(@f)$(find ./zshlib -type f -perm +u=x -maxdepth 1 | awk -F/ '{ print $NF }')}")
 autoload -Uz "${funcNames[@]}"
