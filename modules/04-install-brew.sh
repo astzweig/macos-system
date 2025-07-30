5a52f479bc1f53e82e6e4229aa33e33caa5e71a1
T. R. Bernstein <137705289+trbernstein@users.noreply.github.com>
2025-07-30 09:39:37 +0200

    Fix sysadminctl new boundaries for User ID

--- modules/04-install-brew.sh
+++ modules/04-install-brew.sh
@@ -36,8 +36,8 @@ function ensureUserCanRunPasswordlessSudo() {
 }
 
 function getFirstFreeRoleAccountID() {
-	local minUserID=450
-	local maxUserID=499
+	local minUserID=200
+	local maxUserID=400
 	dscl . -list '/Users' UniqueID | grep '_.*' | sort -n -k2 | awk -v i=${minUserID} '$2>='${minUserID}' && $2<'${maxUserID}' {if(i < $2) { print i; nextfile} else i=$2+1;} END {if(i <= '${maxUserID}' && ($2 < '${minUserID}' || $2 > '${maxUserID}')) print i;}'
 }
 
